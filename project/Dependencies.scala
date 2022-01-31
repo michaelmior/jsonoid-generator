2f1c914124f34e76d59b03644d7afec0ba94a012
Michael Mior <mmior@cs.rit.edu>
2022-01-31 14:14:09 -0500

    Update jsonoid-discovery dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   // Runtime
   lazy val scopt               = "com.github.scopt" %% "scopt"             % "4.0.1"
-  lazy val jsonoid             = "edu.rit.cs"       %% "jsonoid-discovery" % "0.5.2"
+  lazy val jsonoid             = "edu.rit.cs"       %% "jsonoid-discovery" % "0.5.3"
 
   // Test
   lazy val scalaTest           = "org.scalatest" %% "scalatest"         % "3.2.10"
